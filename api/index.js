ba39625a8ef619172355e58d91dc05d59c3b441b
Aayush Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2024-03-10 00:27:37 +0300

    Change API route from GET to POST

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require("express"); const app = express();
 let value = false;
-app.get("/toggle", (req, res) => {value = !value;res.send(value);});
+app.post("/toggle", (req, res) => {value = !value;res.send(value);});
 app.get("/", (req, res) => {res.header("Access-Control-Allow-Origin", "*");res.send(value);});
 app.listen(3000, () => {console.log(`Server listening at http://localhost:3000`);});
